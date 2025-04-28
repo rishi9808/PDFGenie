e5c7977189b06117379d7355e799da2f4e271f5b
Rishikesh K V <rishikeshkaroth@gmail.com>
2025-04-28 17:06:20 +0530

    feat: implement user creation functionality and integrate with Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
-import { UserButton } from "@clerk/nextjs";
+"use client"
+import { api } from "@/convex/_generated/api";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { useEffect } from "react";
 
 
 export default function Home() {
+  const {  user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    const checkUserExists = async () => {
+      if (user) {
+        const res = await createUser({
+          email: user?.primaryEmailAddress?.emailAddress ?? "",
+          imageUrl: user?.imageUrl ?? "",
+          userName: user?.fullName ?? ""
+        });
+        console.log(res)
+      }
+    };
+    
+    if (user) {
+      checkUserExists();
+    }
+  }, [user, createUser])
+
   return (
     <div>
       <UserButton />
+
+      <h1 className="text-3xl font-bold underline">
+        {user ? `Hello ${user?.firstName}` : `hi`}
+      </h1>
     </div>
  
   );
---
+++ convex/user.ts
@@ -0,0 +1,39 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+
+export const createUser = mutation({
+    args: {
+        userName: v.string(),
+        email: v.string(),
+        imageUrl: v.string()
+    },
+    handler: async(ctx,args) => {
+        // check for null values
+        if (args.email === "" || args.imageUrl === "" || args.userName === "") {
+            throw new ConvexError("user details cannot be empty")
+        }
+        // check if user already exist
+        const user = await ctx.db.query('users')
+        .filter((q)=> q.eq(q.field('email'), args.email))
+        .collect()
+
+        if (user.length === 0){
+            try {
+                await ctx.db.insert("users",
+                    {
+                        email: args.email,
+                        imageUrl: args.imageUrl,
+                        username: args.userName
+                    }
+                )
+
+                return "user created successfully"
+            } catch (error) {
+                throw new ConvexError(`error inserting userdetails, ${error}`)
+            }
+        }
+
+        return "User already exists"
+    }
+})
